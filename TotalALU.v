813c41702c52206b1388298442a5fe93ac221cf2
Tony0 <tony0@example.com>
2026-06-01 21:43:40 +0800

    update project

--- TotalALU.v
+++ TotalALU.v
@@ -18,20 +18,15 @@ parameter MULTU= 6'b011001;
 parameter MFHI= 6'b010000;
 parameter MFLO= 6'b010010;
 
-wire [5:0]  SignaltoALU ;
-wire [5:0]  SignaltoSHT ;
-wire [5:0]  SignaltoMUL ;
-wire [5:0]  SignaltoMUX ;
 wire [31:0] ALUOut, HiOut, LoOut, ShifterOut ;
 wire [31:0] dataOut ;
 wire [63:0] Mul_Ans ;
 
-ALUControl ALUControl( .Signal(Signal), .SignaltoALU(SignaltoALU), .SignaltoSHT(SignaltoSHT), .SignaltoMUL(SignaltoMUL), .SignaltoMUX(SignaltoMUX) );
-ALU ALU( .dataA(dataA), .dataB(dataB), .Signal(SignaltoALU), .dataOut(ALUOut), .reset(reset) );
-Multiplier Multiplier( .clk(clk), .dataA(dataA), .dataB(dataB), .Signal(SignaltoMUL), .dataOut(Mul_Ans), .reset(reset) );
-Shifter Shifter( .dataA(dataA), .dataB(dataB), .Signal(SignaltoSHT), .dataOut(ShifterOut), .reset(reset) );
+ALU ALU( .dataA(dataA), .dataB(dataB), .Signal(Signal), .dataOut(ALUOut), .reset(reset) );
+Multiplier Multiplier( .clk(clk), .dataA(dataA), .dataB(dataB), .Signal(Signal), .dataOut(Mul_Ans), .reset(reset) );
+Shifter Shifter( .dataA(dataA), .dataB(dataB), .Signal(Signal), .dataOut(ShifterOut), .reset(reset) );
 HiLo HiLo( .clk(clk), .MulAns(Mul_Ans), .HiOut(HiOut), .LoOut(LoOut), .reset(reset) );
-MUX MUX( .ALUOut(ALUOut), .HiOut(HiOut), .LoOut(LoOut), .Shifter(ShifterOut), .Signal(SignaltoMUX), .dataOut(dataOut) );
+MUX MUX( .ALUOut(ALUOut), .HiOut(HiOut), .LoOut(LoOut), .Shifter(ShifterOut), .Signal(Signal), .dataOut(dataOut) );
 
 assign Output = dataOut ;
 
